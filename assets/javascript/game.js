f1ccc0d000aa730677b515d4e8477ddbe6d9ebff
John Carver <29667140+JCppe@users.noreply.github.com>
2019-07-31 22:11:35 -0400

    split array logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,26 +8,32 @@ var guessesLeft = 12;
 winsHTML.innerHTML = wins;
 guessesHTML.innerHTML = guessesLeft;
 
+var wordArray = ["one", "two", "three", "four"];
+var computerWord;
+var splitWord = [];
+reset();
+
 document.onkeyup = function (event) {
 
     var userGuess = event.key.toLowerCase();
 
     guessesListHTML.append(userGuess + " ");
 
-
-    //need to add the real logic
-    if (userGuess === "0") {
-        wins++;
-        alert("You did it!");
-        winsHTML.innerHTML = wins;
-        reset();
-    } else if (guessesLeft === 0) {
-        alert("You're out of guesses! You lose!")
-        reset();
-    } else {
-        guessesLeft--;
-        guessesHTML.innerHTML = guessesLeft;
+    for (var i = 0; i < splitWord.length; i++) {
+        if (userGuess === "0") {
+            wins++;
+            alert("You did it!");
+            winsHTML.innerHTML = wins;
+            reset();
+        } else if (guessesLeft === 0) {
+            alert("You're out of guesses! You lose!")
+            reset();
+        } else {
+            guessesLeft--;
+            guessesHTML.innerHTML = guessesLeft;
+        }
     }
+
 }
 
 
@@ -35,4 +41,7 @@ function reset() {
     guessesLeft = 12;
     guessesHTML.innerHTML = guessesLeft;
     guessesListHTML.innerHTML = "";
+    computerWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+    splitWord = computerWord.split("");
+    console.log(splitWord);
 }
